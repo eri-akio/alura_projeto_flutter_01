9ed160ba6f3ba06b762e10f406c6649f32882289
erik-akio <erikmatsumoto18@gmail.com>
2024-06-16 14:37:06 -0300

    Aula utilizando Column e Row

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,32 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(color: Colors.red, width: 100, height: 100,),
-          Container(color: Colors.blue,width: 50, height: 50),
-        ],
-      ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.red, width: 100, height: 100,),
+              Container(color: Colors.blue,width: 50, height: 50),
+            ],
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.blue, width: 100, height: 100,),
+              Container(color: Colors.red,width: 50, height: 50),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(color: Colors.cyan, height: 50, width: 50,),
+              Container(color: Colors.pink, height: 50, width: 50,),
+              Container(color: Colors.green, height: 50, width: 50,),
+            ],
+          )
+      ],)
     );
   }
 }
