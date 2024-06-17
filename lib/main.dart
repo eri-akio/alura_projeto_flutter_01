2bf874c6de6ef54a04f47c3cda67045d1612136c
eri-akio <erikmatsumoto18@gmail.com>
2024-06-17 16:36:43 -0300

    Utilizando o Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,27 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tarefas'),
         ),
-        body: Container(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Task("Aprender Flutter"),
+            Task("Aprender Dart"),
+            Task("Meditar"),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
           child: Stack(
             children: [
               Container(
@@ -37,16 +57,13 @@ class MyApp extends StatelessWidget {
                       width: 72,
                       height: 100,
                     ),
-                    Text("Aprender Flutter"),
+                    Text(nome),
                     ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
                   ],
                 ),
               ),
             ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
-      ),
-    );
+        );
   }
 }
