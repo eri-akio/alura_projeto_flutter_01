b5cb248957c667607a8cfb3eeed6c6a67c7b14aa
eri-akio <erikmatsumoto18@gmail.com>
2024-06-17 16:48:51 -0300

    Implementando ListView e Padding

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,13 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tarefas'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: ListView(
           children: [
             Task("Aprender Flutter"),
             Task("Aprender Dart"),
+            Task("Meditar"),
+            Task("Meditar"),
+            Task("Meditar"),
             Task("Meditar"),
           ],
         ),
@@ -35,35 +37,44 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatelessWidget {
   final String nome;
-  const Task(this.nome,{super.key});
+  const Task(this.nome, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-          child: Stack(
-            children: [
-              Container(
-                color: Colors.blue,
-                height: 140,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 72,
+                    height: 100,
+                  ),
+                  Container(
+                    width: 200,
+                      child: Text(
+                        nome,
+                        style: TextStyle(fontSize: 24, overflow: TextOverflow.ellipsis,),
+                  )),
+                  ElevatedButton(
+                      onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                ],
               ),
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
-                    Text(nome),
-                    ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
-                  ],
-                ),
-              ),
-            ],
-          ),
-        );
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
