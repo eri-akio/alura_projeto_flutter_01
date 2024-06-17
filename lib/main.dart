3d20eb1652ba2cae8058fa4ddc87a38a8e5e5df7
eri-akio <erikmatsumoto18@gmail.com>
2024-06-17 16:20:35 -0300

    Implementando o layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tarefas'),
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter: Meus primeiros passos'),
+        body: Container(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 140,
+              ),
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Text("Aprender Flutter"),
+                    ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+                  ],
+                ),
+              ),
+            ],
           ),
-          body: Container(color: Colors.black),
-          floatingActionButton: FloatingActionButton(onPressed: (){}),
         ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
     );
   }
 }
