6635a812b0f5189eba8f3ac517460961c37533e7
eri-akio <erikmatsumoto18@gmail.com>
2024-06-18 15:32:09 -0300

    iferenciando Hot Reload e Hot Restart

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
+          leading: Container(),
           title: Text('Tarefas'),
         ),
         body: ListView(
@@ -80,14 +81,25 @@ class _TaskState extends State<Task> {
                               overflow: TextOverflow.ellipsis,
                             ),
                           )),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              nivel++;
-                            });
-                            print(nivel);
-                          },
-                          child: Icon(Icons.arrow_drop_up))
+                      Container(
+                        height: 62,
+                        width: 69,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
+                              print(nivel);
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(Icons.arrow_drop_up),
+                                Text('UP', style: TextStyle(fontSize: 12),),
+                              ],
+                            )),
+                      )
                     ],
                   ),
                 ),
