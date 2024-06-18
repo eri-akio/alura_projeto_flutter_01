ee7668ac7521aef3ea82b07d11d4b68859b52aa9
eri-akio <erikmatsumoto18@gmail.com>
2024-06-18 16:35:25 -0300

    Personalizando a Condição de dificuldade

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,28 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            Task("Aprender Flutter",
-                "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg"),
-            Task("Aprender Dart", "https://blog.codapp.com.br/wp-content/uploads/2023/02/9nmssvliot8mzseyuqdx.png"),
-            Task("Meditar", "https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg"),
-            Task("Jogar", "https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg"),
-            Task("Ler", "https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg"),
-            Task("Andar de bike", "https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+            Task(
+                "Aprender Flutter",
+                "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg",
+                3),
+            Task(
+                "Aprender Dart",
+                "https://blog.codapp.com.br/wp-content/uploads/2023/02/9nmssvliot8mzseyuqdx.png",
+                2),
+            Task(
+                "Meditar",
+                "https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg",
+                1),
+            Task("Jogar",
+                "https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg", 4),
+            Task(
+                "Ler",
+                "https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg",
+                1),
+            Task(
+                "Andar de bike",
+                "https://images.pexels.com/photos/161172/cycling-bike-trail-sport-161172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                5),
           ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -40,8 +55,9 @@ class MyApp extends StatelessWidget {
 class Task extends StatefulWidget {
   final String nome;
   final String foto;
+  final int dificuldade;
 
-  const Task(this.nome, this.foto, {super.key});
+  const Task(this.nome, this.foto, this.dificuldade, {super.key});
 
   @override
   State<Task> createState() => _TaskState();
@@ -91,15 +107,45 @@ class _TaskState extends State<Task> {
                                   overflow: TextOverflow.ellipsis,
                                 ),
                               )),
-                              Row(
-                                children: [
-                                  Icon(Icons.star, size: 15, color: Colors.blue),
-                                  Icon(Icons.star, size: 15, color: Colors.blue),
-                                  Icon(Icons.star, size: 15, color: Colors.blue),
-                                  Icon(Icons.star, size: 15, color: Colors.blue[100]),
-                                  Icon(Icons.star, size: 15, color: Colors.blue[100]),
-                                ],
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 1)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
                               ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 2)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 3)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 4)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 5)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                            ],
+                          ),
                         ],
                       ),
                       Container(
@@ -135,7 +181,9 @@ class _TaskState extends State<Task> {
                       child: Container(
                         child: LinearProgressIndicator(
                           color: Colors.white,
-                          value: nivel / 10,
+                          value: (widget.dificuldade > 0)
+                              ? (nivel / widget.dificuldade) / 10
+                              : 1,
                         ),
                         width: 200,
                       ),
