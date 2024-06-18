502575ddf25f29dee8790c21a545a4fdf3086ef6
eri-akio <erikmatsumoto18@gmail.com>
2024-06-18 16:20:39 -0300

    Adicionando dificuldade na tarefa

--- lib/main.dart
+++ lib/main.dart
@@ -78,15 +78,30 @@ class _TaskState extends State<Task> {
                           fit: BoxFit.cover,
                         ),
                       ),
-                      Container(
-                          width: 200,
-                          child: Text(
-                            widget.nome,
-                            style: TextStyle(
-                              fontSize: 24,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          )),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                              width: 200,
+                              child: Text(
+                                widget.nome,
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )),
+                              Row(
+                                children: [
+                                  Icon(Icons.star, size: 15, color: Colors.blue),
+                                  Icon(Icons.star, size: 15, color: Colors.blue),
+                                  Icon(Icons.star, size: 15, color: Colors.blue),
+                                  Icon(Icons.star, size: 15, color: Colors.blue[100]),
+                                  Icon(Icons.star, size: 15, color: Colors.blue[100]),
+                                ],
+                              ),
+                        ],
+                      ),
                       Container(
                         height: 62,
                         width: 69,
