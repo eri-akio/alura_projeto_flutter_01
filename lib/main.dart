c5bd790b905bca9550171b62a7e2fad3db194cdf
eri-akio <erikmatsumoto18@gmail.com>
2024-06-19 14:09:06 -0300

    Refatorando Difficulty

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:primeiro_projeto/difficulty.dart';
 
 void main() {
   runApp(MyApp());
@@ -136,44 +137,8 @@ class _TaskState extends State<Task> {
                                   overflow: TextOverflow.ellipsis,
                                 ),
                               )),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 1)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 2)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 3)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 4)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 5)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                            ],
+                          Difficulty(
+                            dificultyLevel: widget.dificuldade,
                           ),
                         ],
                       ),
@@ -237,3 +202,4 @@ class _TaskState extends State<Task> {
     );
   }
 }
+
