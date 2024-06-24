169059429b04c21d92aefebc52992b23119bd37c
eri-akio <erikmatsumoto18@gmail.com>
2024-06-24 11:32:10 -0300

    errobuilder

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -29,6 +29,8 @@ class _FormScreenState extends State<FormScreen> {
             border: Border.all(width: 3),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -84,7 +86,10 @@ class _FormScreenState extends State<FormScreen> {
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10),
                   child: Image.network(
-                    (imageController.text.isEmpty) ? "https://via.placeholder.com/150" : imageController.text,
+                    imageController.text,
+                    errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace){
+                      return Image.asset("images/nophoto.png");
+                    },
                     fit: BoxFit.cover,
                   ),
                 ),
