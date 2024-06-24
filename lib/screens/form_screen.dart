873111fcb33c3b14775f9ba5dc9174130ff1c5e2
eri-akio <erikmatsumoto18@gmail.com>
2024-06-24 09:25:25 -0300

    TextFormField

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -26,7 +26,18 @@ class _FormScreenState extends State<FormScreen>
           ),
           child: Column(
             children: [
-              
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Nome',
+                    fillColor: Colors.white70,
+                    filled: true,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
