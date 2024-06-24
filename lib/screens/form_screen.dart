7a4c059d9a7d71da0b452c21ba612c17538dcb4f
eri-akio <erikmatsumoto18@gmail.com>
2024-06-24 16:50:41 -0300

    Aplicando a navegação direta

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -131,6 +131,7 @@ class _FormScreenState extends State<FormScreen> {
                             content: Text('Salvando nova Tarefa'),
                           ),
                         );
+                        Navigator.pop(context);
                       }
                     },
                     child: Text("Adicionar!"),
--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:primeiro_projeto/components/task.dart';
+import 'package:primeiro_projeto/screens/form_screen.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
@@ -9,56 +10,41 @@ class InitialScreen extends StatefulWidget {
 }
 
 class _InitialScreenState extends State<InitialScreen> {
-  bool opacidade = true;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: Container(),
-          title: const Text('Tarefas', style: TextStyle(color: Colors.white),),
-          backgroundColor: Colors.blue,
+      appBar: AppBar(
+        leading: Container(),
+        title: const Text(
+          'Tarefas',
+          style: TextStyle(color: Colors.white),
         ),
-        body: AnimatedOpacity(
-          opacity: opacidade ? 1 : 0,
-          duration: const Duration(milliseconds: 800),
-          child: ListView(
-            children: const [
-              Task(
-                  "Aprender Flutter",
-                  "images/01flutter.jpg",
-                  3),
-              Task(
-                  "Aprender Dart",
-                  "images/02dart.png",
-                  2),
-              Task(
-                  "Meditar",
-                  "images/03meditar.jpeg",
-                  1),
-              Task("Jogar",
-                  "images/04jogar.jpg", 4),
-              Task(
-                  "Ler",
-                  "images/05ler.jpg",
-                  1),
-              Task(
-                  "Andar de bike",
-                  "images/06bike.jpeg",
-                  5),
-              SizedBox(
-                height: 100,
-              )
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          setState(() {
-            opacidade = !opacidade;
-          });
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView(
+        children: const [
+          Task("Aprender Flutter", "images/01flutter.jpg", 3),
+          Task("Aprender Dart", "images/02dart.png", 2),
+          Task("Meditar", "images/03meditar.jpeg", 1),
+          Task("Jogar", "images/04jogar.jpg", 4),
+          Task("Ler", "images/05ler.jpg", 1),
+          Task("Andar de bike", "images/06bike.jpeg", 5),
+          SizedBox(
+            height: 100,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => FormScreen(),
+            ),
+          );
         },
-          child: const Icon(Icons.remove_red_eye),
-        ),
-      );
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
